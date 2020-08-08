faa4271c70809d8b733fc600b0f038493d2c98ec
netbriler <netbriler@gmail.com>
2020-08-08 13:44:01 +0300

    Card adding & deleting action

--- src/components/menu-list/menu-list.js
+++ src/components/menu-list/menu-list.js
@@ -1,8 +1,9 @@
-import React, {Component} from 'react';
+
+import React, { Component } from 'react';
 import MenuListItem from '../menu-list-item';
 import { connect } from 'react-redux';
 import WithRestoService from '../hoc';
-import {menuLoaded, menuRequested, menuError} from '../../actions';
+import { menuLoaded, menuRequested, menuError, addedToCart } from '../../actions';
 import Spinner from '../spinner';
 import Error from '../error';
 
@@ -13,33 +14,35 @@ class MenuList extends Component {
     componentDidMount() {
         this.props.menuRequested();
 
-        const {RestoService} = this.props;
+        const { RestoService } = this.props;
         RestoService.getMenuItems()
             .then(res => this.props.menuLoaded(res))
-            .catch(error => this.props.menuError());
+            .catch(() => this.props.menuError());
     }
 
     render() {
-        const {menuItems, loading, error} = this.props;
-        if (error){
-            return <Error/>
+        const { menuItems, loading, error, addedToCart } = this.props;
+        if (error) {
+            return <Error />
         }
         if (loading) {
-            return <Spinner/>
+            return <Spinner />
         }
         const items = menuItems.map(menuItem => {
-                return (
-                    <MenuListItem key = {menuItem.id} menuItem = {menuItem}/>
-                )
-            })
+            return (<MenuListItem
+                key={menuItem.id}
+                menuItem={menuItem}
+                onAddToCart={() => addedToCart(menuItem.id)} />
+            )
+        })
 
         return (
-            <View items = {items}/> 
-            )
+            <View items={items} />
+        )
     }
 };
 
-const mapStateToProps =  (state) =>{
+const mapStateToProps = (state) => {
     return {
         menuItems: state.menu,
         loading: state.loading,
@@ -51,17 +54,18 @@ const mapStateToProps =  (state) =>{
 const mapDispatchToProps = {
     menuLoaded: menuLoaded,
     menuRequested,
-    menuError
+    menuError,
+    addedToCart
 }
 
 
-const View = ({items}) => {
+const View = ({ items }) => {
 
     return (
         <ul className="menu__list">
             {items}
         </ul>
-    ) 
+    )
 }
 
-export default WithRestoService ()( connect(mapStateToProps, mapDispatchToProps)(MenuList) );
+export default WithRestoService()(connect(mapStateToProps, mapDispatchToProps)(MenuList));
